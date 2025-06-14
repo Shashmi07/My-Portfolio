b525f980e696e29774c7985f93b96d4d8540de49
Shashmi07 <shashmiamadi225@gmail.com>
2025-06-14 13:41:21 +0530

    add all

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import logo from "../assets/TTVLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+ 
+const Navbar = () => {
+  return (
+    <nav className=" mb-20 flex items-center justify-between py-6">
+    <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-15" src={logo} alt="logo" />
+    </div>
+    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/shashmi-amadi-8b0584306/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/Shashmi07"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <FaSquareXTwitter />
+        <FaInstagram />
+    </div>
+  </nav>
+  );
+  
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = ` I am an  Information Technology undergraduate at the Institute of Technology, University of Moratuwa (ITUM), with a strong passion for front-end development and quality assurance (QA). I have hands-on experience with front-end technologies such as React, along with a foundational understanding of back-end frameworks like Node.js and PHP.  I also work with database like  MySQL. Through coursework and practical projects, I continue to enhance my skills in building responsive, user-friendly web interfaces while ensuring high-quality standards through effective testing and QA practices.`;
+
+export const ABOUT_TEXT_1 = `I am a dedicated and adaptable front-end developer with a growing interest in quality assurance (QA) and web application testing. As an Information Technology undergraduate at the Institute of Technology, University of Moratuwa (ITUM), I have gained hands-on experience through academic projects and self-learning, working with technologies such as React, Node.js, PHP, and MySQL. I'm passionate about creating responsive, user-friendly interfaces and ensuring their reliability through effective QA practices. Eager to learn and improve, I thrive in collaborative environments and continuously explore modern development tools, frameworks, and testing methodologies to enhance both development and software quality.`;
+
+
+
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Dehigahawaththa,Nadugala,Matara ",
+  phoneNo: "+94715406511 ",
+  email: "shashmiamadi225@gmail.com",
+};
